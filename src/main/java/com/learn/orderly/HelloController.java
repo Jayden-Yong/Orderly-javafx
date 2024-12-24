4552367964b5a051bb0f7938a39566da5d0f08c3
Jayden-Yong <jaydenyong28@gmail.com>
2024-12-25 01:31:21 +0800

    first commit

---
+++ src/main/java/com/learn/orderly/HelloController.java
@@ -0,0 +1,14 @@
+package com.learn.orderly;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
